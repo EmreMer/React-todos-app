d5b7d176600f3a11d612be4fffa3c9cb27fc4880
EMRE MERZİFONLUOĞLU <emremer91@yahoo.com.tr>
2022-09-02 12:39:07 +0300

    debug

--- src/Components/Todos/buttons/Button.jsx
+++ src/Components/Todos/buttons/Button.jsx
@@ -1,48 +1,67 @@
-import React from 'react'
+import React from "react";
 
-function Buttons({ todos, addTodos, activeTodos, setActiveTodos, filteredTodos }) {
-    const clearItems = () => {
-        addTodos([]);
-    };
-    return (
-        <footer class="footer">
+function Buttons({
+  todos,
+  addTodos,
+  activeTodos,
+  setActiveTodos,
+  filteredTodos,
+}) {
+  const clearItems = () => {
+    addTodos([]);
+  };
+  return (
+    <footer class="footer">
+      <span className="todo-count">
+        <strong>{filteredTodos.length}</strong> items left{" "}
+      </span>
 
-            <span className="todo-count"><strong>{filteredTodos.length}</strong> items left </span>
+      <ul className="filters">
+        <li>
+          <a
+            href="#"
+            onClick={(e) => {
+              setActiveTodos("All");
+            }}
+            className={activeTodos === "All" ? "selected" : undefined}
+          >
+            All
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            onClick={(e) => {
+              setActiveTodos("Active");
+            }}
+            className={activeTodos === "Active" ? "selected" : undefined}
+          >
+            Active
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            onClick={(e) => {
+              setActiveTodos("Completed");
+            }}
+            className={activeTodos === "Completed" ? "selected" : undefined}
+          >
+            Completed
+          </a>
+        </li>
+      </ul>
 
-            <ul className="filters">
-                <li>
-                    <a
-                        onClick={(e) => { setActiveTodos("All") }}
-                        className={activeTodos === "All" ? 'selected' : undefined}
-                    >All</a>
-                </li>
-                <li>
-                    <a
-                        onClick={(e) => { setActiveTodos("Active") }}
-                        className={activeTodos === "Active" ? 'selected' : undefined}
-                    >Active</a>
-                </li>
-                <li>
-                    <a
-                        onClick={(e) => { setActiveTodos("Completed") }}
-                        className={activeTodos === "Completed" ? 'selected' : undefined}
-                    >Completed</a>
-                </li>
-            </ul>
-
-
-            <button
-                className="clear-completed"
-                onClick={clearItems}>
-                Clear completed
-            </button>
-            <footer className="info">
-                <p>Click to edit a todo</p>
-                <p>Created by <a href="https://d12n.me/">Dmitry Sharabin</a></p>
-                <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
-            </footer>
-        </footer>
-    )
+      <button className="clear-completed" onClick={clearItems}>
+        Clear completed
+      </button>
+      <footer className="info">
+        <p>Click to edit a todo</p>
+        <p>Created by Dmitry Sharabin</p>
+        <p>Part of TodoMVC</p>
+      </footer>
+    </footer>
+  );
 }
 
-export default Buttons
+export default Buttons;
